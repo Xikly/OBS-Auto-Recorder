d77677d29ce9aa33d555d8d8433dd7c43e9b39b2
NagromW <107074986+NagromW@users.noreply.github.com>
2022-08-25 17:48:22 +0200

    Added start.bat for windows, added timestamp printing to main script.

--- autorecord.py
+++ autorecord.py
@@ -1,6 +1,7 @@
 import processcheck
 import pydirectinput
 import time
+from datetime import datetime
 from re import search
 
 scanamount = 1
@@ -29,26 +30,29 @@ for x in processlistraw:
 
 #PROGRAM BEGINNING
 while processcheck.checkIfProcessRunning("obs64.exe") is True:
-    print ('Scanning. Program has scanned {0} time/s.'.format(scanamount))
+    print ('{0} | Scanning for process to open. Script has scanned {1} time/s.'.format(datetime.fromtimestamp(datetime.timestamp(datetime.now())), scanamount))
 
     scanamount = scanamount + 1
 
     for x in processlist:
         if processcheck.checkIfProcessRunning(x) is True:
-            print ('Process running!')
+            print ('{0} | Process running!'.format(datetime.fromtimestamp(datetime.timestamp(datetime.now()))))
             pydirectinput.press(hotkey)
             y = True
+            scanamount = 1
             while y is True:
+                print ('{0} | Scanning for process closure. Script has scanned {1} time/s.'.format(datetime.fromtimestamp(datetime.timestamp(datetime.now())), scanamount))
                 if processcheck.checkIfProcessRunning(x) is False:
-                    print ('program exitted')
+                    print ('{0} | Process exitted.'.format(datetime.fromtimestamp(datetime.timestamp(datetime.now()))))
                     scanamount = 1
                     pydirectinput.press(hotkey)
                     y = False
                 else:
                     time.sleep(2)
+                    scanamount = scanamount + 1
     
     time.sleep(5)
 
 pnfile.close()
 
-print ('OBS IS NOT OPEN!\nOPEN OBS FOR THE PROGRAM TO FUNCTION.')
+print ('{0} | OBS IS NOT OPEN!\nOPEN OBS FOR THE SCRIPT TO FUNCTION.'.format(datetime.fromtimestamp(datetime.timestamp(datetime.now()))))
